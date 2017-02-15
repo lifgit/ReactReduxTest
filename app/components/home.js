090b793cd7badcc836851d6e9af74b51b5ca08fc
feizhi <feizhi@oscgc.com>
2017-02-14 23:56:43 -0500

    1rst commit

---
+++ app/components/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = React.createClass({
+  render: function() {
+    return (
+      <div className="home-page">
+        <h1>The app is now using Redux</h1>
+        <p>
+          Contents in homepage. BLABLABLABLABLABLBALBLABLABLABLABLABLBALBLABLABLABLABLABLBAL
+        </p>
+      </div>
+    );
+  }
+});
+
+export default Home;
